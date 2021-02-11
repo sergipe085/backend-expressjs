24279378f35639f3d3bf256ee5ff1d309e1522cb
Sérgio <71034184+Cangaceiro15@users.noreply.github.com>
2021-02-10 22:10:32 -0300

    Query, Route and Body params study

--- src/index.js
+++ src/index.js
@@ -2,8 +2,57 @@ const express = require('express');
 
 const app = express();
 
-app.get('/helloworld', (request, response) => { 
-  return response.json({ message: "Hello World" });
+app.use(express.json());
+
+//Busca informacoes no backend
+app.get('/projects', (request, response) => { 
+  const query = request.query;
+
+  console.log(query);
+
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3'
+  ]);
 });
 
-app.listen(3333); 
+//adiciona informacoes no backend
+app.post('/projects', (request, response) => {
+  const body = request.body;
+
+  console.log(body);
+
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3',
+    'Projeto 4'
+  ]);
+});
+
+//altera informacoes no backend
+app.put('/projects/:id', (request, response) => {
+  const params = request.params;
+
+  console.log(params);
+
+  return response.json([
+    'Projeto 4',
+    'Projeto 3',
+    'Projeto 2',
+    'Projeto 1'
+  ]);
+});
+
+//deleta informacoes no backend
+app.delete('/projects/:id', (request, response) => {
+  return response.json([
+    'Projeto 4',
+    'Projeto 3',
+  ]);
+})
+
+app.listen(3333, () => {
+  console.log("🚀 backend started!");
+}); 
